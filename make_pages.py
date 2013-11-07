911a88ffc7a553ac592ad2e9669c07de0fec5150
mtytel <matthewtytel@gmail.com>
2013-11-07 11:22:10 -0800

    Added termite page.

--- make_pages.py
+++ make_pages.py
@@ -5,35 +5,38 @@ import jinja2
 
 TARGET = 'to-deploy/'
 
-pages = [ {
-    'filename': 'index.html',
-    'context':  {
-        'active_tab': '/',
-    }
-} ]
+pages = [
+  {
+    'template': 'index.html',
+    'path': '',
+    'tabname': 'Little LFO',
+  },
+  {
+    'template': 'termite.html',
+    'path': 'termite',
+    'tabname': 'Termite',
+  },
+]
 
 def main():
-    here = os.path.dirname(__file__)
-    deploy_target = os.path.join(here, TARGET)
-    loader = jinja2.FileSystemLoader(os.path.join(here, 'templates'))
-    templates = jinja2.Environment(loader=loader)
+  here = os.path.dirname(__file__)
+  deploy_target = os.path.join(here, TARGET)
+  loader = jinja2.FileSystemLoader(os.path.join(here, 'templates'))
+  templates = jinja2.Environment(loader=loader)
 
-    if os.path.exists(deploy_target):
-        shutil.rmtree(deploy_target)
-    os.makedirs(deploy_target)
-    shutil.copytree(os.path.join(here, 'static'), os.path.join(deploy_target, 'static'))
+  if os.path.exists(deploy_target):
+    shutil.rmtree(deploy_target)
+  os.makedirs(deploy_target)
+  shutil.copytree(os.path.join(here, 'static'), os.path.join(deploy_target, 'static'))
 
-    for page in pages:
-        tem = templates.get_template(page['filename'])
-        ctx = page['context']
-        if page['filename'] == 'index.html':
-            target = os.path.join(deploy_target)
-        else:
-            os.makedirs(os.path.join(deploy_target, page['route']))
-            target = os.path.join(deploy_target, page['route'])
-        with codecs.open(os.path.join(target, 'index.html'), 'w') as out:
-            out.write(tem.render(**ctx))
-
+  for page in pages:
+    tem = templates.get_template(page['template'])
+    ctx = { 'pages': pages , 'current_page': page }
+    out_dir = os.path.join(deploy_target, page['path'])
+    if page['path']:
+      os.makedirs(out_dir)
+    with codecs.open(os.path.join(out_dir, 'index.html'), 'w') as out:
+      out.write(tem.render(**ctx))
 
 if __name__ == '__main__':
-    main()
+  main()
